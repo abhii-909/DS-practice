3e765b8027daa999bbc170ae06b82f17b9b7f30f
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2024-09-08 23:38:04 +0530

    Time: O(n^2).......Space: O(1)

---
+++ Sorting/selection_sort.java
@@ -0,0 +1,35 @@
+//Problem Statement: Given an array of N integers, write a program to implement the Selection sorting algorithm.
+
+
+public class selection_sort{
+    static void select_sort(int arr[] , int n){
+        for (int i = 0; i < n-1; i++) {
+            int mini = i;
+            for (int j = i; j < n; j++) {
+                if(arr[j] < arr[mini]){
+                    mini = j;
+                }
+            }
+
+            //SWAP -->
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println("After selection sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        System.out.println("Before selection sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        select_sort(arr, n);
+    }
+}
